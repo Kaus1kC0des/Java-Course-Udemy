2129f7e5a6003c7bd647ba885b2cadf619ee4556
Kausik D <kausikdevanathan@gmail.com>
2025-06-08 22:02:28 +0530

    Notes on Strings in Java

---
+++ src/StringTutorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class StringTutorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+//        Finding an element at index
+        System.out.print("Enter the position: ");
+        int pos = sc.nextInt();
+        System.out.println("Element at position " + pos + " is " + str.charAt(pos));
+
+//        Other String methodss
+    }
+}
