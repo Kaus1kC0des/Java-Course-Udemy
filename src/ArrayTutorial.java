f93a12e551d88b259454e80dcb097ff213b6bdf9
Kausik D <kausikdevanathan@gmail.com>
2025-06-20 18:32:18 +0530

    Revisited the DataTypes and Arrays in Java

--- src/ArrayTutorial.java
+++ src/ArrayTutorial.java
@@ -1,5 +1,5 @@
-import java.util.*;
-import java.util.stream.DoubleStream;
+import java.util.Arrays;
+import java.util.Random;
 
 public class ArrayTutorial {
 
---
+++ src/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a = (byte) 23523523;
+        int b = 23523523;
+        System.out.println('c' - 'a');
+        System.out.println(a);
+    }
+}
